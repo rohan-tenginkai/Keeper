a444a4186b063ddead8d2705d260ee906ac51d73
RohanTenginkai <rohan02022002@gmail.com>
2022-04-16 09:02:18 +0530

    Initial Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+
+function Header() {
+  return (
+    <header>
+      <h1><CalendarMonthIcon /> Keeper</h1>
+    </header>
+  );
+}
+
+export default Header;
